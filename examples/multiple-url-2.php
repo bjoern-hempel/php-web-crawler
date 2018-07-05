ae38e15fcedd0af3b471f5319310f18b8af123e4
Björn Hempel <bjoern@hempel.li>
2018-07-06 00:49:12 +0200

    add new multiple url parser

---
+++ examples/multiple-url-2.php
@@ -0,0 +1,97 @@
+<?php
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Björn Hempel <bjoern@hempel.li>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Ixno\WebCrawler;
+
+include dirname(__FILE__).'/../autoload.php';
+
+use Ixno\WebCrawler\Converter\DateParser;
+use Ixno\WebCrawler\Converter\Sprintf;
+use Ixno\WebCrawler\Converter\Trim;
+use Ixno\WebCrawler\Output\Field;
+use Ixno\WebCrawler\Output\Group;
+use Ixno\WebCrawler\Source\Url;
+use Ixno\WebCrawler\Source\XpathSection;
+use Ixno\WebCrawler\Value\XpathTextnode;
+use Ixno\WebCrawler\Source\File;
+use Ixno\WebCrawler\Source\XpathSections;
+
+//$source = dirname(__FILE__).'/html/praesidium.html';
+//$url = 'https://www.adac.de/der-adac/verein/management/praesidium/';
+//
+//$html = new File(
+//    $source,
+//    new Field('title', new XpathTextnode('//*[@id="l_main_content"]/h1')),
+//    new Group(
+//        'hits',
+//        new XpathSections(
+//            '//*[@id="l_main_content"]/div/div[3]/div/div[1]/figure',
+//            new Field('name', new XpathTextnode('./figcaption/div/div[1]', new Trim())),
+//            new Field('position', new XpathTextnode('./figcaption/div/div[2]', new Trim())),
+//            new Field('image', new XpathTextnode('./img/@src', new Sprintf('https://www.adac.de%s')))
+//        )
+//    )
+//);
+
+//$source = dirname(__FILE__).'/html/verwaltungsrat.html';
+//$url = 'https://www.adac.de/der-adac/verein/management/verwaltungsrat/';
+//
+//$html = new File(
+//    $source,
+//    new Field('title', new XpathTextnode('//*[@id="l_main_content"]/h1')),
+//    new Group(
+//        'hits',
+//        new XpathSections(
+//            '//*[@id="l_main_content"]/div/div[3]/div/div[1]/figure',
+//            new Field('name', new XpathTextnode('./figcaption/div/div[2]', new Trim())),
+//            new Field('area', new XpathTextnode('./figcaption/div/div[1]', new Trim())),
+//            new Field('image', new XpathTextnode('./img/@src', new Sprintf('https://www.adac.de%s')))
+//        )
+//    )
+//);
+
+$source = dirname(__FILE__).'/html/geschaeftsfuehrung.html';
+$url = 'https://www.adac.de/der-adac/verein/management/geschaeftsfuehrung/';
+
+$html = new File(
+    $source,
+    new Field('title', new XpathTextnode('//*[@id="l_main_content"]/h1')),
+    new Group(
+        'hits',
+        new XpathSections(
+            '//*[@id="l_main_content"]/div[contains(concat(" ", normalize-space(@class), " "), " cf ")]',
+            new Field('name', new XpathTextnode('./div/div[2]/p', new Trim())),
+            new Field('area', new XpathTextnode('./h2', new Trim())),
+            //*[@id="l_main_content"]/div[2]/div/div[1]/figure/div/picture/img
+            new Field('image', new XpathTextnode('./div/div[1]/figure/div/picture/img/@data-src', new Sprintf('https://www.adac.de%s')))
+        )
+    )
+);
+
+$data = json_encode($html->parse(), JSON_PRETTY_PRINT);
+
+print_r($data);
+
+echo "\n";
